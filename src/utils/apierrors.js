4e59553ad8b0a9509dbde2bfd9822f4c24e50705
vaishnavi <vaishnavislambe75@gmail.com>
2024-08-10 10:18:31 +0530

    utils code

---
+++ src/utils/apierrors.js
@@ -0,0 +1,26 @@
+class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        statck = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = errors
+
+        // this is to get track of the files which have problems
+        if(statck){
+            this.stack = statck
+        }
+        else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+
+    }
+}
+
+export {ApiError}
