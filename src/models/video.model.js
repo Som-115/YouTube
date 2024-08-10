8d70826d962ca1911ecb3f797cfaefd4002acb04
vaishnavi <vaishnavislambe75@gmail.com>
2024-08-10 11:47:55 +0530

    created user and video models

---
+++ src/models/video.model.js
@@ -0,0 +1,49 @@
+/*
+model link: https://app.eraser.io/workspace/YtPqZ1VogxGy1jzIDkzj
+*/
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongoose, {Schema} from "mongoose";
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, // cloudnary url
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number, // from cloudnary 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
